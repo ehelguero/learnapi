11658522db3e504248a4749e6f503fa86ce7c341
loupgarou81 <leloupgarou81@gmail.com>
2016-07-10 23:36:22 +0100

    Created server.js and configured express

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+// config = require('./server/configure')
+var app = express();
+
+// Configure app
+app.set('port', process.env.PORT || 3300);
+app.set('views', __dirname + '/views');
+
+// Pass app to function config
+// app = config(app);
+
+// Configure main route
+app.get('/', function(req,res){
+  res.send('Hello World');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Server listening to port http://localhost:' + app.get('port'))
+})
