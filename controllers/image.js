15214256900df3cdee7844ce157e92e18215e522
loupgarou81 <leloupgarou81@gmail.com>
2016-07-13 01:01:29 +0100

    Finished upload function

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,6 @@
+var fs = require('fs'),
+    path = require('path');
+
 module.exports = {
   index: function(req,res){
     var viewModel = {
@@ -32,7 +35,38 @@ module.exports = {
     res.render('image', viewModel);
   },
   create: function(req,res){
-    res.send('Create');
+    console.log(req.file)
+    // function to upload an image
+    var saveImage = function(){
+      // characters for generate filename
+      var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
+          imgUrl = '';
+      // generate random name
+      for(var i=0;i<6;i++){
+        imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+
+      // get file temp path, extension and target
+      var tempPath = req.file.path,
+          ext = path.extname(req.file.originalname).toLowerCase(),
+          targetPath = path.resolve('./public/upload/' + imgUrl + ext);
+          
+      if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        fs.rename(tempPath, targetPath, function(err) {
+          if(err) throw err;
+          // redirect to the page with the image
+          res.redirect('/images/' + imgUrl);
+        })
+      } else {
+        // delete image temp
+        fs.unlink(tempPath, function(err) {
+          if (err) throw err;
+          // send info 500
+          res.status(500).json({error: 'Only image files are allowed.'});
+        });
+      }
+    };
+    saveImage();
   },
   like: function(req,res){
     res.send('Hola');
